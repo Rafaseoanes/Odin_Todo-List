949917ec2ec66427886e92cedc8df75eb79b241f
rafaseoanes <rafaseoanes9@gmail.com>
2024-07-11 14:49:22 -0500

    Modules initialized

---
+++ src/todo-constructor.js
@@ -0,0 +1,11 @@
+class Todo {
+  constructor(title, description, dueDate, priority, project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+    this.project = project;
+  }
+  updateTodo() {}
+}
