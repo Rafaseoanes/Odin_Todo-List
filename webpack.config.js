d737cae09a2faae3cb38f8d2122048842f26f3fb
rafaseoanes <rafaseoanes9@gmail.com>
2024-07-11 13:50:37 -0500

    Project setup updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/**/*.html", "src/**/*"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  // optimization: {
+  //   runtimeChunk: "single",
+  // },
 };
